a512bad61f011eb6f76a8866233a13c3c432ed1c
Germain <germain.gg90@gmail.com>
2024-05-31 15:22:59 -0400

    fonts addred

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { titleFont } from "@/config/fonts";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <main>
+      <h1 className={`${titleFont.className} font-bold `}>Hola Mundo</h1>
+    </main>
+  );
 }
 
 export default Home;

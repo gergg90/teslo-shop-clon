7647312467f0c62105cf9a591c55bac2272e9a8b
Germain <germain.gg90@gmail.com>
2024-05-31 22:20:14 -0400

    not found category addred

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,5 +1,17 @@
-function CategoryPage() {
-  return <div>CategoryPage</div>;
+import { notFound } from "next/navigation";
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+function CategoryPage({ params }: Props) {
+  const { id } = params;
+
+  if (id === "kids") notFound();
+
+  return <div>CategoryPage {id}</div>;
 }
 
 export default CategoryPage;
---
+++ src/app/(shop)/category/not-found.tsx
@@ -0,0 +1,7 @@
+import PageNotFound from "@/components/ui/not-found/PageNotFound";
+
+function NotFoundCategoryPage() {
+  return <PageNotFound />;
+}
+
+export default NotFoundCategoryPage;

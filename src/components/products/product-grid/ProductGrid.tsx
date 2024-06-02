8813790db3c89193d26f8275018aa819d77578f3
Germain <germain.gg90@gmail.com>
2024-06-02 08:40:00 -0400

    ProductGridItem addred

--- src/components/products/product-grid/ProductGrid.tsx
+++ src/components/products/product-grid/ProductGrid.tsx
@@ -1,4 +1,5 @@
 import { Product } from "@/interfaces";
+import { ProductGriItem } from "./ProductGriItem";
 
 interface Props {
   products: Product[];
@@ -8,7 +9,7 @@ export function ProductGrid({ products }: Props) {
   return (
     <div className="grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10">
       {products.map((product) => (
-        <span key={product.slug}> {product.title} </span>
+        <ProductGriItem key={product.slug} product={product} />
       ))}
     </div>
   );

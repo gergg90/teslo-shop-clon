92e0d4f710b8a326bf18f57d7dddcc20f42a80f6
Germain <germain.gg90@gmail.com>
2024-06-09 00:03:32 -0400

    carrito de compras ui listo

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -7,6 +7,9 @@ const productInCart = [
   initialData.products[0],
   initialData.products[1],
   initialData.products[2],
+  initialData.products[4],
+  initialData.products[5],
+  initialData.products[6],
 ];
 
 function CartPage() {
@@ -21,32 +24,64 @@ function CartPage() {
             <Link href="/" className="underline mb-15">
               Continua comprando
             </Link>
+
+            {/* Items */}
+
+            {productInCart.map((product) => (
+              <div key={product.slug} className="flex mt-2 mb-2">
+                <Image
+                  src={`/products/${product.images[0]}`}
+                  alt={product.title}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                  }}
+                  width={100}
+                  height={100}
+                  className="mr-5 rounded"
+                />
+
+                <div>
+                  <p>{product.title}</p>
+                  <p className="mb-2">${product.price}</p>
+
+                  <QuantitySelector quantity={1} />
+
+                  <button className="underline mt-3">Remover</button>
+                </div>
+              </div>
+            ))}
           </div>
 
-          {/* Items */}
+          {/* Checkout - resumen de la oreden */}
 
-          {productInCart.map((product) => (
-            <div key={product.slug} className="flex">
-              <Image
-                src={`/products/${product.images[0]}`}
-                alt={product.title}
-                width={100}
-                height={100}
-                className="mr-5 rounded"
-              />
+          <div className="bg-white rounded-xl h-fit shadow-xl p-10">
+            <h2 className="text-2xl mb-2">Resumen de orden:</h2>
+            <div className="grid grid-cols-2">
+              <span>N° productos</span>
+              <span className="text-right">3 productos</span>
 
-              <div>
-                <p>{product.title}</p>
-                <p className="mb-2">${product.price}</p>
+              <span>Sub total</span>
+              <span className="text-right">$ 300</span>
 
-                <QuantitySelector quantity={1} />
+              <span>Iva (16%)</span>
+              <span className="text-right">$ 48</span>
 
-                <button className="underline mt-3">Remover</button>
-              </div>
+              <span className="font-extrabold mt-5 text-2xl">Total:</span>
+              <span className="text-right font-extrabold mt-5 text-2xl">
+                $ 348
+              </span>
             </div>
-          ))}
 
-          {/* Checkout */}
+            <div className="mt-7 w-full">
+              <Link
+                href="/checkout/address"
+                className="flex underline decoration-white underline-offset-2 justify-center btn-primary"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>

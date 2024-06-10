12964e51ddd0b4cd34eb44dda2c3dbd91873b53a
Germain <germain.gg90@gmail.com>
2024-06-09 21:59:15 -0400

    carrito vacio

--- src/app/(shop)/empty/page.tsx
+++ src/app/(shop)/empty/page.tsx
@@ -1,5 +1,18 @@
+import Link from "next/link";
+import { IoCartOutline } from "react-icons/io5";
+
 function EmptyPage() {
-  return <div>EmptyPage</div>;
+  return (
+    <div className="flex justify-center items-center h-[800px]">
+      <IoCartOutline size={80} className="mx-5" />
+      <div className="flex flex-col items-center">
+        <h1 className="text-xl font-semibold">Tu carrito esta vacio</h1>
+        <Link href="/" className="mt-2 text-4xl text-blue-600">
+          Compra ahora
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default EmptyPage;
